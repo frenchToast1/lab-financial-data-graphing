3f0c7ca8e3a72660ed8bba122c301159657ad0ab
hassan sabeh <h.adamsabeh@yahoo.com>
2021-11-27 15:02:11 +0100

    iteration 1 done

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,15 @@
+const buttonEl = document.querySelector("#get-info-button");
+
+
+buttonEl.addEventListener("click", function() {
+  axios.get("http://api.coindesk.com/v1/bpi/historical/close.json")
+    .then(function(bitcoinData) {
+      console.log(bitcoinData)
+    })
+    .catch(err => console.log(err))
+});
+
+
 document.addEventListener(
   "DOMContentLoaded",
   () => {
