62a14977eb14534eab0fd14e33fb1f33907b2bff
hassan sabeh <h.adamsabeh@yahoo.com>
2021-11-27 15:31:40 +0100

    iteration 2 Done

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,29 @@ buttonEl.addEventListener("click", function() {
   axios.get("http://api.coindesk.com/v1/bpi/historical/close.json")
     .then(function(bitcoinData) {
       console.log(bitcoinData)
+      const labels = Object.keys(bitcoinData.data.bpi)
+      const data = {
+        labels: labels,
+        datasets: [{
+          label: 'My First dataset',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: Object.values(bitcoinData.data.bpi),
+        }]
+      };
+      
+      const config = {
+        type: 'line',
+        data: data,
+        options: {}
+      };
+      const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+      );
+
+
+
     })
     .catch(err => console.log(err))
 });
